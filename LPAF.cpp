c8ab7989d9414438e2332aa532424a2a6f628d26
burntsaxphonereed <151691868+burntsaxphonereed@users.noreply.github.com>
2024-06-16 20:03:16 -0400

    Update LPAF.cpp

--- LPAF.cpp
+++ LPAF.cpp
@@ -2,4 +2,11 @@ class CfgPatches
 {
 	class ANP_alysian__National_Police_main
 	{ 
-     "
+           units[]=
+          {
+              "ANP_Soldier",
+              "ANP_Soldier_c
+              " 
+              " 
+              " 
+              "
